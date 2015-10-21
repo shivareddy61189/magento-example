430bd92d3305b16c9b5b4216ece6647387c16f65
shivareddy61189 <shivapratapreddy@yahoo.com>
2015-10-21 11:57:52 +0530

    Update index.php

--- php/index.php
+++ php/index.php
@@ -79,7 +79,7 @@ if (isset($_SERVER['MAGE_IS_DEVELOPER_MODE'])) {
 umask(0);
 
 /* Store or website code */
-$mageRunCode = isset($_SERVER['MAGE_RUN_CODE']) ? $_SERVER['MAGE_RUN_CODE'] : '';
+$mageRunCode = isset($_SERVER['MAGE_RUN_CODE']) ? $_SERVER['MAGE_RUN_CODE'] : 'si';
 
 /* Run store or run website */
 $mageRunType = isset($_SERVER['MAGE_RUN_TYPE']) ? $_SERVER['MAGE_RUN_TYPE'] : 'store';
